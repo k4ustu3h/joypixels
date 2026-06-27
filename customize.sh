cf72c1467463e687c347bf3499d2d2ab5fefb787
Kaustubh Ladiya <k4ustu3h@gmail.com>
2026-06-27 12:54:30 +0530

    JoyPixels® 11.0

--- customize.sh
+++ customize.sh
@@ -3,8 +3,7 @@ ui_print "- Searching in fonts.xml"
 [[ -d /sbin/.core/mirror ]] && MIRRORPATH=/sbin/.core/mirror || unset MIRRORPATH
 FILE=/system/etc/fonts.xml
 FILENAME=$(sed -ne '/<family lang="und-Zsye".*>/,/<\/family>/ {s/.*<font weight="400" style="normal">\(.*\)<\/font>.*/\1/p;}' $MIRRORPATH$FILE)
-for i in $FILENAME
-do
-ui_print "- Copying font files to $i"
-cp -f $MODPATH/system/fonts/JoyPixels.ttf $MODPATH/system/fonts/$i
+for i in $FILENAME; do
+	ui_print "- Copying font files to $i"
+	cp -f $MODPATH/system/fonts/JoyPixels.ttf $MODPATH/system/fonts/$i
 done
